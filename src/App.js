23b914118c25a787f2feedbe62f7b3b6ebccaaef
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-28 20:46:01 -0700

    finished vending machine exercise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Chips from './Chips';
-import Sardines from './Sardines';
-import Soda from './Soda';
-import VendingMachine from './VendingMachine'
-import './App.css';
+import React, { Component } from "react";
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import Navbar from "./Navbar";
+import VendingMachine from "./VendingMachine";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" render={() => <VendingMachine />} />
-        <Route exact path="/chips" render={() => <Chips />} />
-        <Route exact path="/sardines" render={() => <Sardines />} />
-        <Route exact path="/soda" render={() => <Soda />} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' render={() => <VendingMachine />} />
+          <Route exact path='/soda' render={() => <Soda />} />
+          <Route exact path='/sardines' render={() => <Sardines />} />
+          <Route exact path='/chips' render={() => <Chips />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
